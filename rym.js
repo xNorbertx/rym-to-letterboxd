dc6d812b8763cdc24d41a2a140f7594cdae8ca37
Norbert Bakker <n.bakker@betabit.nl>
2019-01-30 12:33:06 +0100

    added username, added three columns (still empty), randomly skips pages

--- rym.js
+++ rym.js
@@ -1,11 +1,15 @@
 const puppeteer = require('puppeteer');
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+const username = process.argv[2];
 const csvWriter = createCsvWriter({
     path: './csv-output.csv',
     header: [
-        {id: 'title', title: 'TITLE'},
-        {id: 'year', title: 'YEAR'},
-        {id: 'rating', title: 'RATING'}
+        {id: 'title', title: 'Title'},
+        {id: 'year', title: 'Year'},
+        {id: 'rating', title: 'Rating'},
+        {id: 'director', title: 'Director'},
+        {id: 'date', title: 'WatchedDate'},
+        {id: 'tags', title: 'Tags'}
     ]
 });
 
@@ -27,8 +31,11 @@ async function parseFilmInfo(page, pagenumber) {
             for (let i = 0; i < numberOfVotes; i++) {
                 movie = {
                     title: votes[i].querySelector('a').innerText,
-                    year: votes[i].querySelector('span.smallgray').innerText.slice(1, -1),
-                    rating: votes[i].parentElement.previousElementSibling.getElementsByTagName('img')[0].title.substring(0, 4)
+                    year: votes[i].querySelectorAll('span.smallgray')[0].innerText.slice(1, -1),
+                    rating: votes[i].parentElement.previousElementSibling.getElementsByTagName('img')[0].title.substring(0, 4),
+                    director: '',
+                    date: '',
+                    tags: ''
                 }
                 movies.push(movie);
             }
@@ -45,7 +52,7 @@ function run () {
             const browser = await puppeteer.launch();
             const page = await browser.newPage();
             let moviesTotal = [];
-            await page.goto("https://rateyourmusic.com/film_collection/euroshopper/recent/");
+            await page.goto("https://rateyourmusic.com/film_collection/"+username+"/recent/");
             page.title().then(function(res) {
                 if( res === "IP blocked") {
                     return reject("IP is blocked");
@@ -56,19 +63,16 @@ function run () {
                 return parseInt(nodes[nodes.length-1].innerText);
             });
             let moviesFromPage = [];
-            for (let i = 1; i < 10+1; i++) {
-                await page.goto("https://rateyourmusic.com/film_collection/euroshopper/recent/"+i);
+            for (let i = 1; i < numberOfPages+1; i++) {
+                await page.goto("https://rateyourmusic.com/film_collection/"+username+"/recent/"+i);
                 moviesFromPage = await parseFilmInfo(page, i);
                 moviesTotal = moviesTotal.concat(moviesFromPage);
                 //Need to wait a while between requests, because RYM is sensitive towards bots
                 await sleep(randomize(10000) + 60000);  
             }
             browser.close();
-            csvWriter.writeRecords(moviesTotal)
-                .then(() => {
-                    console.log('DONE!');
-                });
-            return resolve();
+            csvWriter.writeRecords(moviesTotal);
+            return resolve("Done");
         } catch (e) {
             return reject(e);
         }
